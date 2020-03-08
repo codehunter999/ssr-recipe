fa5a67119aa37aed0098859e25bb0f9e8f8771db
codehunter999 <49275951+codehunter999@users.noreply.github.com>
2020-03-08 19:45:37 +0900

    서버사이드렌더링 일차 커밋

--- config/webpack.config.server.js
+++ config/webpack.config.server.js
@@ -58,10 +58,10 @@ module.exports = {
           {
             test: cssRegex,
             exclude: cssModuleRegex,
-            //  exportOnlyLocals: true 옵션을 설정해야 실제 css 파일을 생성하지 않습니다.
+            //  onlyLocals: true 옵션을 설정해야 실제 css 파일을 생성하지 않습니다.
             loader: require.resolve("css-loader"),
             options: {
-              exportOnlyLocals: true
+              onlyLocals: true
             }
           },
           // CSS Module 을 위한 처리
@@ -70,7 +70,7 @@ module.exports = {
             loader: require.resolve("css-loader"),
             options: {
               modules: true,
-              exportOnlyLocals: true,
+              onlyLocals: true,
               getLocalIdent: getCSSModuleLocalIdent
             }
           },
@@ -82,7 +82,7 @@ module.exports = {
               {
                 loader: require.resolve("css-loader"),
                 options: {
-                  exportOnlyLocals: true
+                  onlyLocals: true
                 }
               },
               require.resolve("sass-loader")
@@ -97,7 +97,7 @@ module.exports = {
                 loader: require.resolve("css-loader"),
                 options: {
                   modules: true,
-                  exportOnlyLocals: true,
+                  onlyLocals: true,
                   getLocalIdent: getCSSModuleLocalIdent
                 }
               },
